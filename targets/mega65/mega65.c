b1f716e9f9b01fd332dbc000b1d2b45111279416
gurcei <gurce.isikyildiz@gmail.com>
2018-02-17 11:19:58 +1100

    A modest attempt at adding a hardware 'N' (next / step over) command. Works ok-ish, but fails on special calls such as JSR checkpoint, which fiddle with the stack and don't return via RTS (they JMP back).

--- targets/mega65/mega65.c
+++ targets/mega65/mega65.c
@@ -52,6 +52,8 @@ static char fast_mhz_in_string[8];
 static int frame_counter;
 static int   paused = 0, paused_old = 0;
 static int   breakpoint_pc = -1;
+static int   orig_sp = 0;
+static int   trace_next_trigger = 0;
 static int   trace_step_trigger = 0;
 static void (*m65mon_callback)(void) = NULL;
 static const char emulator_paused_title[] = "TRACE/PAUSE";
@@ -469,6 +471,19 @@ void m65mon_set_trace ( int m )
 	paused = m;
 }
 
+void m65mon_do_next ( void )
+{
+	if (paused) {
+		umon_send_ok = 0; // delay command execution!
+		m65mon_callback = m65mon_show_regs; // register callback
+		trace_next_trigger = 3; // if JSR, then trigger until RTS to next_addr
+    orig_sp = cpu65.sphi | cpu65.s;
+		paused = 0;
+	} else {
+		umon_printf(SYNTAX_ERROR "trace can be used only in trace mode");
+	}
+}
+
 void m65mon_do_trace ( void )
 {
 	if (paused) {
@@ -483,6 +498,11 @@ void m65mon_do_trace ( void )
 void m65mon_do_trace_c ( void )
 {
 	umon_printf(SYNTAX_ERROR "command 'tc' is not implemented yet");
+}
+void m65mon_next_command ( void )
+{
+  if (paused)
+    m65mon_do_next();
 }
 
 void m65mon_empty_command ( void )
@@ -600,7 +620,32 @@ int main ( int argc, char **argv )
 	if (!xemucfg_get_bool("syscon"))
 		sysconsole_close(NULL);
 	xemu_timekeeping_start();
+
 	for (;;) {
+
+			if (trace_next_trigger == 3)
+			{
+				trace_next_trigger--;
+			}
+			else if (trace_next_trigger == 2)
+			{
+				if (cpu65.op == 0x20)
+					trace_next_trigger = 1;
+				else
+				{
+					trace_next_trigger = 0;
+					paused = 1;
+				}
+			}
+			else if (trace_next_trigger == 1)
+			{
+				if ((cpu65.sphi | cpu65.s) == orig_sp )
+				{
+					trace_next_trigger = 0;
+					paused = 1;
+				}
+			}
+
 		while (XEMU_UNLIKELY(paused)) {	// paused special mode, ie tracing support, or something ...
 			if (XEMU_UNLIKELY(dma_status))
 				break;		// if DMA is pending, do not allow monitor/etc features
