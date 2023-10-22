bd15184706a432851884500fcbc24c1419213230
Gurce Isikyildiz <Gurce.Isikyildiz@resmed.com.au>
2023-10-22 16:55:07 +1100

    fixed a bug in my rebase effort

--- targets/mega65/uart_monitor.c
+++ targets/mega65/uart_monitor.c
@@ -227,6 +227,7 @@ static void execute_command ( comms_details_type *cd, char *cmd )
 			bank = par1 >> 16;
 			if (cmd && check_end_of_command(cmd, 1)) {
 				for (int k = 0; k < 16; k++)
+        {
 					if (bank == 0x777)
 						m65mon_dumpmem16(par1);
 					else
